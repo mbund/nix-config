6c0476302b12907ad7bd3b07779370d37fdae4b9
mbund <25110595+mbund@users.noreply.github.com>
2022-07-03 23:03:07 -0400

    mount root btrfs subvolume in partition script

--- hardware/hajimaru/partition.sh
+++ hardware/hajimaru/partition.sh
@@ -57,6 +57,7 @@ btrfs subvolume create /mnt/nix/swap
 umount /mnt/nix
 
 # create swapfile
+mount -o subvol=root,compress=zstd,noatime /dev/mapper/nix /mnt/nix
 mkdir -p /mnt/nix/swap
 mount -o subvol=swap,compress=none,noatime /dev/mapper/nix /mnt/nix/swap
 truncate -s 0 /mnt/nix/swap/swapfile
