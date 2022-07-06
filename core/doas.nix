b0a977980e2d5ab786b17dee7299641ff252c318
mbund <25110595+mbund@users.noreply.github.com>
2022-07-05 20:27:23 -0400

    make sudo default again

--- core/doas.nix
+++ core/doas.nix
@@ -1,5 +1,5 @@
 {
-  security.sudo.enable = false;
+  # security.sudo.enable = false;
   security.doas.enable = true;
   security.doas.extraRules = [
     { groups = [ "wheel" ]; noPass = false; keepEnv = true; }
