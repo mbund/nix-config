3848c363742ebaba7a016f59958ff8a10f01f29c
mbund <25110595+mbund@users.noreply.github.com>
2022-07-06 23:08:35 -0400

    add syncs to hajimaru partitioner

--- hardware/hajimaru/partition.sh
+++ hardware/hajimaru/partition.sh
@@ -15,9 +15,11 @@ set -ex
 sgdisk --zap-all $dev
 
 sgdisk -n 0:0:+2GiB -c 0:boot -t 0:ef00 $dev
+sync
 mkfs.vfat -F 32 -n boot /dev/disk/by-partlabel/boot
 
 sgdisk -n 0:0:0 -c 0:root $dev
+sync
 cryptsetup luksFormat --batch-mode --type=luks2 --label=root /dev/disk/by-partlabel/root /dev/zero --keyfile-size=1
 cryptsetup luksOpen --batch-mode /dev/disk/by-partlabel/root root --key-file=/dev/zero --keyfile-size=1
 
