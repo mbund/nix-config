dfab2a8ebe1e0b9910e70a5310a3695888fa13f6
mbund <25110595+mbund@users.noreply.github.com>
2022-07-08 00:16:45 -0400

    remove xdg mimeapps configuration

--- users/mbund/firefox.nix
+++ users/mbund/firefox.nix
@@ -72,17 +72,4 @@ in
     MOZ_USE_XINPUT2 = "1";
     MOZ_DBUS_REMOTE = "1";
   };
-
-  xdg.mimeApps.defaultApplications = {
-    "application/x-extension-htm" = "firefox.desktop";
-    "application/x-extension-html" = "firefox.desktop";
-    "application/x-extension-shtml" = "firefox.desktop";
-    "application/x-extension-xht" = "firefox.desktop";
-    "application/x-extension-xhtml" = "firefox.desktop";
-    "application/xhtml+xml" = "firefox.desktop";
-    "text/html" = "firefox.desktop";
-    "x-scheme-handler/chrome" = "firefox.desktop";
-    "x-scheme-handler/http" = "firefox.desktop";
-    "x-scheme-handler/https" = "firefox.desktop";
-  };
 }
