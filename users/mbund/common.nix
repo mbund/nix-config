2430e457c3feb936ae1d7362e15b7b22fde14137
mbund <25110595+mbund@users.noreply.github.com>
2022-07-12 14:54:59 -0400

    automatically ignore home-manager file collisions

--- users/mbund/common.nix
+++ users/mbund/common.nix
@@ -1,8 +1,15 @@
-{ host, ... }:
+{ config, lib, host, ... }:
 {
   home.sessionVariables = {
     NIX_CONFIG_DIR =
       if host == "kodai" then "$HOME/data/nix-config"
       else null;
+
+    HOME_MANAGER_BACKUP_EXT = "hm-remove";
   };
+
+  # remove all files ending with .hm-remove
+  home.activation.removeOverwrite = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    $DRY_RUN_CMD find ${config.home.homeDirectory} -type f -name "*.hm-remove" -exec rm {} \;
+  '';
 }
