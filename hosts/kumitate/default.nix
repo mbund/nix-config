202129532cec1dfe11c1b9371eb721adc4840e95
mbund <25110595+mbund@users.noreply.github.com>
2022-09-01 01:05:53 -0400

    kumitate: remove tlp charge threshold

--- hosts/kumitate/default.nix
+++ hosts/kumitate/default.nix
@@ -22,8 +22,6 @@
   powerManagement.powertop.enable = true;
   services.tlp.enable = true;
   services.tlp.settings = {
-    START_CHARGE_THRESH_BAT0 = 90;
-    STOP_CHARGE_THRESH_BAT0 = 97;
     PCIE_ASPM_ON_BAT = "powersupersave";
     RUNTIME_PM_ON_BAT = "auto";
   };
