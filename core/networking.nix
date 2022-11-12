e08550d7b48fad7a8aeb3880ebb942ad65d7dbb6
mbund <25110595+mbund@users.noreply.github.com>
2022-11-12 12:24:02 -0500

    remove networking configuration

--- core/networking.nix
+++ core/networking.nix
@@ -25,19 +25,19 @@
   networking.wireguard.enable = true;
   networking.networkmanager.enable = true;
 
-  services.resolved.enable = true;
-  services.resolved.dnssec = "true";
-  services.resolved.extraConfig = ''
-    MulticastDNS=true
-    DNSOverTLS=yes
-  '';
-  networking.nameservers = [
-    "1.1.1.1"
-    # "2620:fe::fe#dns.quad9.net"
-    # "2620:fe::9#dns.quad9.net"
-    # "9.9.9.9#dns.quad9.net"
-    # "149.112.112.112#dns.quad9.net"
-  ];
+  # services.resolved.enable = true;
+  # services.resolved.dnssec = "true";
+  # services.resolved.extraConfig = ''
+  #   MulticastDNS=true
+  #   DNSOverTLS=yes
+  # '';
+  # networking.nameservers = [
+  #   "1.1.1.1"
+  #   # "2620:fe::fe#dns.quad9.net"
+  #   # "2620:fe::9#dns.quad9.net"
+  #   # "9.9.9.9#dns.quad9.net"
+  #   # "149.112.112.112#dns.quad9.net"
+  # ];
 
   services.tailscale.enable = true;
   systemd.services.tailscaled.after = ["network-online.target" "systemd-resolved.service"];
