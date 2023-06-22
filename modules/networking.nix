28e0cc473d151717311f587418f71887db148a85
Mark Bundschuh <mark@mbund.org>
2023-06-22 17:41:40 -0400

    remove nftables default

--- modules/networking.nix
+++ modules/networking.nix
@@ -24,4 +24,20 @@
   networking.firewall.allowedUDPPorts = [
     41641
   ];
+
+  # networking.nftables.enable = true;
+  # networking.nftables.ruleset = ''
+  #   table inet mullvad-ts {
+  #   chain exclude-outgoing {
+  #     type route hook output priority 0; policy accept;
+  #     ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+  #     ip6 daddr fd7a:115c:a1e0::/48 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+  #   }
+  # 
+  #   chain allow-incoming {
+  #     type filter hook input priority -100; policy accept;
+  #     iifname "tailscale0" ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+  #   }
+  # }
+  # '';
 }
