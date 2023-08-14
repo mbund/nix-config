48628a58d042166bb8e7dc3a3e60be7d4d844e92
Mark Bundschuh <bundschuhmark@gmail.com>
2023-08-14 11:43:19 -0400

    fix formatting

--- modules/networking.nix
+++ modules/networking.nix
@@ -33,7 +33,7 @@
   #     ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
   #     ip6 daddr fd7a:115c:a1e0::/48 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
   #   }
-  # 
+  #
   #   chain allow-incoming {
   #     type filter hook input priority -100; policy accept;
   #     iifname "tailscale0" ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
